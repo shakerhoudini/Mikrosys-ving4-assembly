ff44be14a58781e8a2af54be8afd81dbbaf61f0a
shakerhoudini <71225776+shakerhoudini@users.noreply.github.com>
2023-02-14 14:28:44 +0100

    Update oppgave2.asm

--- oppgave2.asm
+++ oppgave2.asm
@@ -2,6 +2,8 @@
 
 #include <AVR128DB48def.inc>
 
+
+//a)
 	; 127 + 129
 ldi r16, 127
 ldi r17, 129
@@ -21,3 +23,22 @@ add r20, r21  ; Satt SREG-flagget V, fordi summen overstiger 8-biters verdiområ
 ldi r22, 254
 ldi r23, 255
 add r22, r23  ; Satt SREG-flaggene C og V, fordi summen overstiger 8-biters verdiområdet
+
+//b)
+
+ldi r28, low(1000)
+ldi r29, high(1000)
+
+ldi r30, low(500)
+ldi r31, high(500)
+
+add r28, r30
+adc r29, r31
+
+//resultatet ligger nå i r17:r16 
+
+//c)
+
+adiw r29 : r28 , 50 // add value to Y pointer
+
+//Resultatet er korrekt
